0ad2f9864fcbe929b39b8ecbed0bb58b5ca734dc
jxderalx <pontigaalejandro25@gmail.com>
2024-12-10 12:34:37 +0100

    Final Version

--- app/src/main/java/com/apg/knowshareapp/ui/settings_screen/SettingsScreen.kt
+++ app/src/main/java/com/apg/knowshareapp/ui/settings_screen/SettingsScreen.kt
@@ -1,8 +1,9 @@
 package com.apg.knowshareapp.ui.settings_screen
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun SettingsScreen(){
-
+    Text(text = "Bienvenido a Settings Screen")
 }
